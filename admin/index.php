393485734cb551a8b66243f5e6ed715e4bc3e769
Juan Manuel Ortiz <juanmaortyz@gmail.com>
2014-04-05 20:59:46 -0300

    admin login fix

--- admin/index.php
+++ admin/index.php
@@ -1,8 +1,16 @@
 <?php
 
+session_start();
+
 include_once '../init.php';
 include_once ROOT_DIR . '/servicios/servicios.php';
 include_once ROOT_DIR . '/entidades/usuario.php';
+
+$estadoLogin = $_SESSION['estadoLogin'];
+if (isset($estadoLogin) && $estadoLogin) {
+    header("Location: admin_panel.php");
+    return;
+}
 
 $usuario = $_POST['nombre_usuario'];
 $password = $_POST['password_usuario'];
@@ -11,10 +19,10 @@ $servicios = new Servicios();
 $oUsuario = $servicios->getUsuarios($usuario);
 
 if (isset($oUsuario) && $oUsuario->getUser() == $usuario && $oUsuario->getPass() == md5($password)) {
-    echo "Todo bien";
-    $_SESSION['estadoLogin'] == TRUE;
-    $_SESSION['id_usuario'] == $oUsuario->getId();
-    $_SESSION['nombre_usuario'] == $oUsuario->getUser();
+    $_SESSION['estadoLogin'] = TRUE;
+    $_SESSION['id_usuario'] = $oUsuario->getId();
+    $_SESSION['nombre_usuario'] = $oUsuario->getUser();
+    header("Location: admin_panel.php");
 } elseif ($usuario == NULL || $password == NULL) {
     header("Location: admin_login.php");
 } else {
