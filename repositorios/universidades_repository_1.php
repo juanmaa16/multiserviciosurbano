47dc54c1f370ef907517f4514f4aed75c6a1e646
Juan Manuel Ortiz <juanmaortyz@gmail.com>
2014-04-05 20:08:59 -0300

    Corrección en interface de universidades

--- repositorios/universidades_repository_1.php
+++ repositorios/universidades_repository_1.php
@@ -3,8 +3,6 @@
 interface UniversidadesRepository {
     
      public function insertUniversidad(Universidad $oUniversidad);
-     public function updateUniversidad(Universidad $oUniversidad);
-     public function deleteUniversidad(Universidad $oUniversidad);
      public function getUniversidades();
      public function getUniversidadById($idUniversidad);
 }
