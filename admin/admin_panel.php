7dcb52d5ad995eab51425c07ec0fa7f9fe449834
Juan Manuel Ortiz <juanmaortyz@gmail.com>
2014-04-05 21:04:25 -0300

    Panel de administración

--- admin/admin_panel.php
+++ admin/admin_panel.php
@@ -1,16 +1,46 @@
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
-<!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <link rel="stylesheet" type="text/css" href="../css/style.css">
+        <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div id="contenedor">
+            <?php include '../header.php' ?>
+            <div id="contenido">
+                <div style="color:white; padding-left:40px;">
+                    <h1>PANEL ADMINISTRADOR</h1>
+                </div>
+
+                <div id="centro">
+                    <div style="margin-top:40px;margin-bottom: 40px;">
+                        <a href="universidades.php">
+                            <input type="button" class="textbox" value="UNIVERSIDADES">
+                        </a>
+                        <br/>
+                        <br/>
+                        <a href="carreras.php">
+                            <input type="button" class="textbox" value="CARRERAS">
+                        </a>
+                        <br/>
+                        <br/>
+                        <a href="materias.php">
+                            <input type="button" class="textbox" value="MATERIAS">
+                        </a>
+                        <br/>
+                        <br/>
+                        <a href="documentos.php">
+                            <input type="button" class="textbox" value="DOCUMENTOS">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <?php include '../footer.php' ?>
+        </div>
     </body>
 </html>
+
+
+
+
+
+
