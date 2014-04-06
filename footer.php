58680a3a529cb886cdedba6e6cb2d9707ec519bc
Juan Manuel Ortiz <juanmaortyz@gmail.com>
2014-04-06 17:27:02 -0300

    Corrección en texto del footer

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <div id="footer">
-    <ul type="none">
+    <ul>
         <li><img src="images/home.png" width="20px" heigth="20px">Av. Pellegrini 1517</li>
         <li><img src="images/phone.png" width="20px" heigth="20px">0341-444444444</li>
-        <div style="float:right;">
-            <li align="right">Dessarrolado por Appricotweb</li>
+        <div style="float:right; font-size: 11px; margin: 4px;">
+            Dessarrolado por <strong>Appricotweb</strong>
         </div>
     </ul>
 </div>
