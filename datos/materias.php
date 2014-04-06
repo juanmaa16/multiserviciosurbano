3a3899606cddd957f698d495f6abf9f65ce39bad
Juan Manuel Ortiz <juanmaortyz@gmail.com>
2014-04-06 02:18:59 -0300

    Materia: Nuevo metodo

--- datos/materias.php
+++ datos/materias.php
@@ -67,6 +67,24 @@ class DataMaterias extends Data implements MateriasRepository {
         return $vMaterias;
     }
 
+    public function getMateriasByIdCarrera($idCarrera) {
+        $query = "SELECT * FROM materias WHERE id_carrera=? ORDER BY id_materia DESC";
+        $stmt = $this->prepareStmt($query);
+        $stmt->bind_param('i', $idCarrera);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        $vMaterias = array();
+        $index = 0;
+        while ($row = $result->fetch_assoc()) {
+            $oMateria = $this->generaMateria($row);
+            $vMaterias[$index] = $oMateria;
+            $index++;
+        };
+        $stmt->close();
+        return $vMaterias;
+    }
+
     private function generaMateria($row) {
         $oMateria = new Materia();
         $oMateria->setId($row['id_materia']);
--- repositorios/materias_repository.php
+++ repositorios/materias_repository.php
@@ -1,12 +1,18 @@
 <?php
 
 interface MateriasRepository {
-    
-     public function insertMateria(Materia $oMateria);
-     public function updateMateria(Materia $oMateria);
-     public function deleteMateria(Materia $oMateria);
-     public function getMaterias();
-     public function getMateriaById($idMateria);
+
+    public function insertMateria(Materia $oMateria);
+
+    public function updateMateria(Materia $oMateria);
+
+    public function deleteMateria(Materia $oMateria);
+
+    public function getMaterias();
+
+    public function getMateriasByIdCarrera($idCarrera);
+
+    public function getMateriaById($idMateria);
 }
 
 ?>
